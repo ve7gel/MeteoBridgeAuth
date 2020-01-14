861addf74be32255324e8b56af76afe41e74ce63
ve7gel <gordon@the-larsens.ca>
2020-01-14 11:51:31 -0800

    started meteobridge query code-template parser - added wind to node defs
    Signed-off-by: ve7gel <gordon@the-larsens.ca>

--- meteobridgeauth.py
+++ meteobridgeauth.py
@@ -25,16 +25,12 @@ import requests
 import write_profile
 import uom
 
-from getstationdata import extractarray
-
 LOGGER = polyinterface.LOGGER
 """
 polyinterface has a LOGGER that is created by default and logs to:
 logs/debug.log
 You can use LOGGER.info, LOGGER.warning, LOGGER.debug, LOGGER.error levels as needed.
 """
-global temperature, dewpoint, mintemp, maxtemp, rh, minrh, maxrh, wind, solarradiation, et0, rain_today, \
-    pressure, windchill, rain_rate, rain_yesterday, wind_gust, wind_dir
 
 class MBAuthController(polyinterface.Controller):
     #global temperature, dewpoint, mintemp, maxtemp, rh, minrh, maxrh, wind, solarradiation, et0, rain_today, \
@@ -71,7 +67,6 @@ class MBAuthController(polyinterface.Controller):
         pass
 
     def longPoll(self):
-        global mbrdata
 
         # read data
         if self.ip == "":
@@ -109,7 +104,7 @@ class MBAuthController(polyinterface.Controller):
         except:
             LOGGER.error("Failue attempting connect to MeteoBridge device")
 
-            extractarray(mbrdata)
+        getstationdata(mbrdata)
 
         LOGGER.info("Updated data from Meteobridge")
 
@@ -148,7 +143,43 @@ class MBAuthController(polyinterface.Controller):
         )
         return
 
-    def query(self, command=None):
+def getstationdata(mbrcontent):
+        global temperature, dewpoint, mintemp, maxtemp, rh, minrh, maxrh, wind, solarradiation, et0, rain_today, \
+            pressure, windchill, rain_rate, rain_yesterday, wind_gust, wind_dir
+
+        mbrarray = mbrcontent.split(" ")
+
+        lat = float(mbrarray[4])
+        long = float(mbrarray[5])
+
+        temperature = float(mbrarray[0])
+        et0 = float(mbrarray[3])
+        mintemp = float(mbrarray[7])
+        maxtemp = float(mbrarray[6])
+        rh = float(mbrarray[1])
+        minrh = float(mbrarray[9])
+        maxrh = float(mbrarray[8])
+        wind = float(mbrarray[10])
+        # wind = wind / 3.6 # the Meteobridge already reports in mps so conversion is not required
+        solarradiation = float(mbrarray[11])  # needs to be converted from watt/sqm*h to Joule/sqm
+
+        # if solarradiation is not None:
+        #    solarradiation *= 0.0864
+        # LOGGER.debug(str(temperature) + " " + str(et0) + " " + str(mintemp) + " " + str(maxtemp) +
+        #          " " + str(rh) + " " + str(wind) + " " + str(solarradiation))
+
+        rain_today = float(mbrarray[12])
+        dewpoint = float(mbrarray[13])
+        pressure = float(mbrarray[2]) / 10
+        timestamp = int(mbrarray[15])
+        windchill = float(mbrarray[16])
+        # rain_rate = float(mbrarray[17])
+        rain_rate = 17.0
+        rain_yesterday = float(mbrarray[18])
+        wind_gust = float(mbrarray[19])
+        wind_dir = mbrarray[20]
+
+def query(self, command=None):
         self.check_params()
         for node in self.nodes:
             self.nodes[node].reportDrivers()
