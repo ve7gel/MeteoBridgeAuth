655dd199d5ad7034a7513d575a97cc08cf07fb82
ve7gel <gordon@the-larsens.ca>
2020-01-14 15:46:13 -0800

    modify station data acquisition code
    Signed-off-by: ve7gel <gordon@the-larsens.ca>

--- write_profile.py
+++ write_profile.py
@@ -64,7 +64,6 @@ def write_profile(logger, temperature_list, humidity_list, pressure_list,
     if (len(temperature_list) > 0):
         nodedef.write(NODEDEF_TMPL % ('temperature', '139T'))
         nodedef.write("    <sts>\n")
-        logger.debug(temperature_list)
         for t in temperature_list:
             nodedef.write(STATUS_TMPL % (uom.TEMP_DRVS[t], temperature_list[t]))
         nodedef.write("    </sts>\n")
