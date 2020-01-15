cadbe4fe5afd7ec6d49c667b2fc1f601e59379f4
ve7gel <gordon@the-larsens.ca>
2020-01-15 08:44:10 -0800

    add station battery reading
    Signed-off-by: ve7gel <gordon@the-larsens.ca>

--- meteobridgeauth.py
+++ meteobridgeauth.py
@@ -383,7 +383,7 @@ class MBAuthController(polyinterface.Controller):
                  '[mbsystem-longitude]%20[th0temp-dmax]%20[th0temp-dmin]%20[th0hum-dmax]%20' \
                  '[th0hum-dmin]%20[wind0wind-act]%20[sol0rad-act]%20[rain0total-daysum]%20' \
                  '[th0dew-act]%20[UYYYY][UMM][UDD][Uhh][Umm][Uss]%20[epoch]%20[wind0chill-act]%20' \
-                 '[rain0rate-act]%20[rain0total-ydmax]%20[wind0wind-max10]%20[wind0dir-act]%20[uv0index-act]%20[thb0seapress-act]%20[data17num-act]'
+                 '[rain0rate-act]%20[rain0total-ydmax]%20[wind0wind-max10]%20[wind0dir-act]%20[uv0index-act]%20[thb0seapress-act]%20[thb0lowbat-act]'
         return url + values, handler
 
     def getstationdata(self,url,handler):
